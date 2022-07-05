892b2e8b00a29d81c90dd0d9693d8b80bbf47706
Patrick Siqueira <psique.estuda@gmail.com>
2022-07-05 17:03:11 -0300

    Create component Button, Add the library uuid, create add tasks functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import './App.css';
+import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import  {v4 as uuidv4} from 'uuid';
 
 const App = () =>{
-  const [tasks, settasks]=useState([
+  const [tasks, setTasks]=useState([
     {
       id:'1',
       title: 'Estudar programação',
@@ -18,9 +20,18 @@ const App = () =>{
       completed: true,
     }
   ]);
+  const handleTaskAddition = (taskTitle) => {
+    const newTask = [...tasks, {
+      title : taskTitle,
+      id: uuidv4(),
+      completed : false
+    }];
+    setTasks(newTask);
+  }
 
   return <>
   <div className="container">
+    <AddTask handleTaskAddition ={handleTaskAddition}/>
     {<Tasks tasks = {tasks}/>}
   </div>
   </>
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import Button from "./Button";
+import React, { useState } from "react";
+import './AddTask.css';
 
-const AddTask = () => {
-    return <input type="text">
-        
-    </input>;
+const AddTask = ({handleTaskAddition}) => {
+
+    const[InputData, setInputData]=useState("");
+
+    const handleInputChange = (e) => {
+            setInputData(e.target.value);
+    };
+
+    const handleAddTaskClick = () => {
+        handleTaskAddition(InputData);
+        setInputData('');
+    }
+
+    return (<div className="add-task-container">
+        <input 
+        onChange={handleInputChange}
+        value = {InputData}
+        type="text" 
+        className="add-task-input"/>
+        <div className="add-task-button-container">
+        <Button onClick={handleAddTaskClick}>Adiconar</Button>
+        </div>
+    </div>)
 }
 
 export default AddTask;
